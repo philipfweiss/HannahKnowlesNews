0a339c5a15b7b418dcc5ce258bdfc4e7c1ce30e9
philipfweiss <weissp68@stanford.edu>
2016-07-26 16:20:30 -0700

    add phone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160726210712) do
+ActiveRecord::Schema.define(version: 20160726232014) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20160726210712) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "link"
+  end
+
+  create_table "numbers", force: :cascade do |t|
+    t.string   "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
