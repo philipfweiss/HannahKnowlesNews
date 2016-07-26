7fcaa573e39125a6c5d95e081bb9a5be22551589
philipfweiss <weissp68@stanford.edu>
2016-07-25 18:10:25 -0700

    add basic nokogiri

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
-    
+    require 'open-uri'
+    news_tmp_file = open('https://news.google.com')
+    doc = Nokogiri::HTML(news_tmp_file)
+
+    4.times do puts doc end
   end
 
   def help
