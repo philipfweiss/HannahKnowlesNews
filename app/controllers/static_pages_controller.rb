eff56ce0f171967479792ed23658d3fd9a7c061a
philipfweiss <weissp68@stanford.edu>
2016-07-25 18:44:16 -0700

    back to google custom search

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    require 'open-uri'
-    tmp = open('https://www.google.com/?gws_rd=ssl#q=hannah+knowles&tbm=nws&tbs=qdr:m')
-    doc = Nokogiri::HTML(tmp)
-    @content = doc
 
-  end
 
   def help
   end
