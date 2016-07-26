241e794f1d6ecd90ea19f3aa542801ac7bd04ce4
philipfweiss <weissp68@stanford.edu>
2016-07-25 19:14:43 -0700

    comit changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,21 @@
 class StaticPagesController < ApplicationController
   def home
-    @results = GoogleCustomSearchApi.search("Hannah Knowles")
+
+    @merc = []
+    10.times do |x|
+      merc = GoogleCustomSearchApi.search("Hannah Knowles Mercury News", :page => x )
+      merc.items.each do |item|
+        @merc.push item
+      end
+    end
+
+    @daily = []
+    10.times do |x|
+      daily = GoogleCustomSearchApi.search("Hannah Knowles Stanford Daily", :page => x)
+      daily.items.each do |item|
+        @daily.push item
+      end
+    end
   end
 
 
