851268be0094efa1496ae7060db2f757247646e4
philipfweiss <weissp68@stanford.edu>
2016-07-25 22:15:48 -0700

    pipe data to front page for merc

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,21 +2,24 @@ class StaticPagesController < ApplicationController
   def home
 
     @merc = []
-    10.times do |x|
+    1.times do |x|
       merc = GoogleCustomSearchApi.search("Hannah Knowles Mercury News", :page => x )
       merc.items.each do |item|
+        puts item
         @merc.push item
       end
+
     end
 
-    @daily = []
-    10.times do |x|
-      daily = GoogleCustomSearchApi.search("Hannah Knowles Stanford Daily", :page => x)
-      daily.items.each do |item|
-        @daily.push item
-      end
-    end
+     @daily = []
+  #   1.times do |x|
+  #     daily = GoogleCustomSearchApi.search("Hannah Knowles Stanford Daily", :page => x)
+  #     daily.items.each do |item|
+  #       @daily.push item
+  #     end
+  #   end
   end
+
 
 
   def help
