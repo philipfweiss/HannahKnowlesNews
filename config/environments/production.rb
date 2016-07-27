35ee94217363eb5f53d87cf81c822bfc9f604af5
philipfweiss <weissp68@stanford.edu>
2016-07-26 19:17:39 -0700

    update production settings?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,19 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'news.by.hannah.noreply@gmail.com',
+      password:             'fourkids',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
