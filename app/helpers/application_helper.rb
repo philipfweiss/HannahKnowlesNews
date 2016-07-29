9d6e7acf7d888494ce7ef6c10bf2e2bcd0765420
philipfweiss <weissp68@stanford.edu>
2016-07-28 20:48:12 -0700

    add functional social media buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
        page_title + ' | ' + base_title
      end
    end
+
+   def link_to_image(image_path, target_link,options={})
+     link_to(image_tag(image_path, :border => "0"), target_link, options)
+   end
 end
