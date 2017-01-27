329057097cd9075dfb5a00d659a48982a28a008a
philipfweiss <weissp68@stanford.edu>
2017-01-27 11:42:18 -0800

    update smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
       address:              'smtp.gmail.com',
       port:                 587,
       domain:               'gmail.com',
-      user_name:            'news.by.hannah.noreply@gmail.com',
+      user_name:            'hannahnews.noreply@gmail.com',
       password:             'upgrade68',
       authentication:       'plain',
       enable_starttls_auto: true  }
