12023124465a7b5286fd65da519d2dceaf310101
philipfweiss <weissp68@stanford.edu>
2017-01-30 18:52:06 -0800

    letsencrypt

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -28,7 +28,7 @@ class StaticPagesController < ApplicationController
 
   def letsencrypt
     # use your code here, not mine
-    render text: "9q378m_JWtqFSpeETTu6tv8BurdP3jUEm7y33NJS960.qmJD-TSCy_zWbNn1rAmVvL_12D1e-k0llmrEEQvixkc"
+    render text: "oRXHWeApBF7OiXVMObKahfJVPxtp92CdHSYc4MqYv7M.qmJD-TSCy_zWbNn1rAmVvL_12D1e-k0llmrEEQvixkc"
   end
 
 end
